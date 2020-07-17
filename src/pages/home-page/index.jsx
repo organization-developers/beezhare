4f85069ed2b8168a4178aedb8248530514469d1a
Adam Rafiandri <rafiandria23@gmail.com>
2020-07-17 14:40:13 +0700

    Added Font Awesome and Bootstrap 4 CDNs.

--- src/pages/home-page/index.jsx
+++ src/pages/home-page/index.jsx
@@ -5,7 +5,8 @@ import homePageStyles from './home-page.module.scss';
 export default function HomePage() {
   return (
     <>
-      <h1>THIS IS HOME PAGE</h1>
+      <h1 className="text-danger">THIS IS HOME PAGE</h1>
+      <i class="fab fa-500px"></i>
     </>
   );
 }
